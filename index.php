f9001b816c561177cf7aaf48bf43e2d295675920
Cintia Souza <cintiacpereirapereiraesouza@gmail.com>
2025-10-17 11:23:51 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> -
+   <?php
+   $nome ="Calculos Bancarios";
+   $numero1 = 200; 
+   $numero2 = 333;
+   $soma = $numero1 + $numero2;
+   $subtração = $numero1 - $numero2;
+   $multiplicação = $numero1 * $numero2;
+   $divisão = $numero1 / $numero2;
+   echo "Resultado da soma: $soma<br>";
+   echo"Resultado da subtração: $subtração<br>";
+   echo"Resultado da mutltiplicação: $multiplicaação<br>";
+   echo"Resultado da divisão: $divisão<br>";
+   if($soma > 2){
+    echo"o resultado da soma é maior que 2."}
+    elseif($soma == 2){
+    echo"o resultado da divisão é igual exatamente 2."}
+   
+   else ($soma <2){
+    echo"o resultado da divisão é menor que 2."}
+   ?>
 
+</body>
+</html>
